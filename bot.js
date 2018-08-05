a12b55d60ad2e422a2c6ea796dc99324c957e8f6
mohmedabdaala <40897653+mohmedabdaala@users.noreply.github.com>
2018-08-05 12:37:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,23 +4,6 @@ const prefix = '$'
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.on('ready', function(){
-    var ms = 100000 ;
-    var setGame = [`$SYSTEM BOT `,`PLAY IN HD CLAN`];
-    var i = -1;
-    var j = 0;
-    setInterval(function (){
-        if( i == -1 ){
-            j = 1;
-        }
-        if( i == (setGame.length)-1 ){
-            j = -1;
-        }
-        i = i+j;
-        client.user.setGame(setGame[i],`http://www.twitch.tv/KiNg66S`);
-    }, ms);100000
-
-});
 
 client.on('message', message => {
     if (message.content === 'zg') {
