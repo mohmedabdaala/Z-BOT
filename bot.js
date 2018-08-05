12aaa061818d0a179308346a538b8787fb92809c
mohmedabdaala <40897653+mohmedabdaala@users.noreply.github.com>
2018-08-05 15:53:12 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,11 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '$'
-client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-});
+
 
 client.on('message', message => {
     if (message.content === 'zg') {
         message.reply('pong');
       }
-});           
+});
 client.login('temo');
